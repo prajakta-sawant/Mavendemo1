dbfdd53f7ea4d56b77a57d47399485d666e68cac
prasawan <prasawan@LIN51007253.corp.capgemini.com>
2020-09-01 11:17:11 +0530

    initial commit

---
+++ src/main/java/com/cg/project/main/MainClass.java
@@ -0,0 +1,11 @@
+package com.cg.project.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+
+	}
+
+}
---
+++ src/test/java/com/cg/project/AppTest.java
@@ -0,0 +1,22 @@
+package com.cg.project;
+import org.junit.Assert;
+import org.junit.Test;
+import junit.framework.TestCase;
+
+
+public class AppTest 
+    extends TestCase
+{
+   	@Test
+	public void test1() {
+		Assert.fail("test1 has fail");
+	}
+	@Test
+	public void test2() {
+			}
+	@Test
+	public void test3() {
+		Assert.fail("test3 has fail");
+	}
+	
+	}
